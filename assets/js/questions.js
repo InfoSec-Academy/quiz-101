65f1636003b6d2e06dbe3a8e811151ce21344702
ergün bilsel <bilselergun@gmail.com>
2023-05-10 08:59:02 +0300

    quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,223 @@
+var questionArr = [
+    {
+ //QUESTION 1
+    question:  "Aşağıdakilerden hangisi Unix benzeri işletim sistemlerinde sistem açılışının ve işlem yönetiminin başlangıcını yöneten ilk işlem (process) olarak adlandırılır.",
+    firstOption: "process",
+    secondOption: "pid", 
+    thirdOption: "init", 
+    fourthOption: "daemon", 
+    correctAnswer: "#third-option"
+    },     {
+    
+    //QUESTION 2
+    question:  " işlem sonlandırma sinyali alınamadığında veya işlem sonlandırılamadığında, işlem “......” durumuna geçer.",
+    firstOption: "zombie",
+    secondOption: "runable",
+    thirdOption: "kill",
+    fourthOption: "stop",
+    correctAnswer: "#first-option"
+
+},     {
+
+    //QUESTION 3
+    question:  " Torvalds, Helsinki Üniversitesi nde bilgisayar bilimleri öğrencisiydi ve '. . . . . ' adlı bir işletim sistemi üzerinde çalışıyordu",
+    firstOption: "minix",
+    secondOption: "linos",
+    thirdOption: "windows",
+    fourthOption: "unix",
+    correctAnswer: "#first-option"
+
+
+},     {
+    //QUESTION 4
+    question:  " Aşağıdakilerden hangisi Linux’ta en yaygın kullanılan dosya sistemidir. ext3’ün geliştirilmiş bir sürümüdür .",
+    firstOption: "xfs",
+    secondOption: "ext4", 
+    thirdOption: "",
+    fourthOption: "SysInit",
+    correctAnswer: "#second-option"
+
+},        {
+
+    //QUESTION 5
+    question:  "Linux ve diğer işletim sistemlerini önyükleme sürecinde yüklemek için kullanılan bir önyükleme yükleyicisidir",
+    firstOption: "LILO",
+    secondOption: "EFI",
+    thirdOption: "BIOS",
+    fourthOption: "GRUB",
+    correctAnswer: "#fourth-option"
+
+},     {
+
+    //QUESTION 6
+    question: "SGI tarafından geliştirilen ve daha sonra Linux’ta da kullanılmaya başlanan bir dosya sistemidir. Özellikle büyük dosya boyutları, büyük disk alanları ve yüksek performans gerektiren uygulamalar için önerilir.",
+    firstOption: "ext3",
+    secondOption: "xfs",
+    thirdOption: "vfat",
+    fourthOption: "xia", 
+    correctAnswer: "#second-option"
+},     {
+
+
+    //QUESTION 7
+    question:  "Bir dizinden diğerine geçmenizi sağlar",
+    firstOption: "mkdir", 
+    secondOption: "cd", 
+    thirdOption: "print", 
+    fourthOption: "cd", 
+    correctAnswer: "#fourth-option"
+
+},     {
+
+    //QUESTION 8
+    question:  "Bulunduğunuz dizinin tam yolunu gösterir",
+    firstOption: "ls",
+    secondOption: "cd", 
+    thirdOption: "path", 
+    fourthOption: "pwd",
+    correctAnswer: "#fourth-option"
+
+},     {
+
+    //QUESTION 9
+    question:  "Boş bir dizini silmenizi sağlar",
+    firstOption: "mkdir",
+    secondOption: "cp",
+    thirdOption: "mv",
+    fourthOption: "rmdir",
+    correctAnswer: "#fourth-option"
+
+},     {
+
+    //QUESTION 10
+    question: "Bir dosya veya dizini taşımanızı veya yeniden adlandırmanızı sağlar",
+    firstOption: "touch", 
+    secondOption: "cp",
+    thirdOption: "rm",
+    fourthOption: "mv",
+    correctAnswer: "#fourth-option"
+
+},     {
+
+    //QUESTION 11
+    question:  "Bir dosyanın içeriğini görüntüler",
+    firstOption: "cat",
+    secondOption: "echo",
+    thirdOption: "print", 
+    fourthOption: "show",
+    correctAnswer: "#first-option"
+
+
+},     {
+
+    //QUESTION 12
+    question:  "Linux da yeni bir kullanıcı oluşturmak için .... komutu kullanılır",
+    firstOption: "adduserinfo", 
+    secondOption: "useradd",
+    thirdOption: "usradd",
+    fourthOption: "addusr",
+    correctAnswer: "#second-option"
+
+},         {
+
+    //QUESTION 13
+    question:  "Linux’ta bir kullanıcı hesabını silmek için kullanılır .... komutu kullanılır",
+    firstOption: "userdel",
+    secondOption: "userdell",
+    thirdOption: "userdelete",
+    fourthOption: "deleteuser",
+    correctAnswer: "#first-option"
+
+},     {
+
+    //QUESTION 14
+    question:  "'-rwxr--r--' değeri aşadakilerden hangisidir",
+    firstOption: "700",
+    secondOption: "711",
+    thirdOption: "755",
+    fourthOption: "744",
+    correctAnswer: "#fourth-option"
+
+},     {
+
+    //QUESTION 15
+    question:  "'...', Linux ve diğer Unix tabanlı işletim sistemlerinde dosya ve klasör izinlerini değiştirmek için kullanılan bir komuttur.",
+    firstOption: "chroot",
+    secondOption: "chmood",
+    thirdOption: "chmod", 
+    fourthOption: "chown", 
+    correctAnswer: "#third-option"
+
+
+},     {
+
+
+    //QUESTION 16
+    question:  "'...' komutu, Linux ve diğer Unix tabanlı işletim sistemlerinde bir dosyanın veya klasörün sahibini değiştirmek için kullanılır.",
+    firstOption: "chmod", 
+    secondOption: "chown", 
+    thirdOption: "ownch", 
+    fourthOption: "chowner",
+    correctAnswer: "#second-option"
+
+},     {
+
+    //QUESTION 17
+    question:  "'.....', belirli aralıklarla çalıştırılmak üzere önceden planlanmış görevleri yönetmek için kullanılan bir programdır. ",
+    firstOption: "fstab", 
+    secondOption: "mtab", 
+    thirdOption: "crontab",
+    fourthOption: "scheduletab",
+    correctAnswer: "#third-option"
+
+},     {
+    //QUESTION 18
+    question:  "'.......', Unix benzeri işletim sistemlerinde DNS (Domain Name System) yapılandırmasını ayarlamak için kullanılan bir dosyadır. ",
+    firstOption: "/etc/dpkg/dpkg.cfg", 
+    secondOption: "/etc/apt/apt.conf",
+    thirdOption: "/etc/apt/sources.list",
+    fourthOption: "/etc/resolv.conf",
+    correctAnswer: "#fourth-option" 
+
+},     {
+
+
+    //QUESTION 19
+    question:  " Linux çekirdeği tarafından üretilen sistem günlüğü mesajlarını görüntülemek için kullanılan bir komut satırı aracıdır.",
+    firstOption: "mesg",
+    secondOption: "biosmesg", 
+    thirdOption: "dmesg",
+    fourthOption: "ddmesg",
+    correctAnswer: "#third-option"
+
+},     {
+
+    //QUESTION 20
+    question:  "Red Hat ve diğer bazı Linux dağıtımları tarafından kullanılır ve paketlerin kurulumu, kaldırılması, güncellenmesi ve yönetimi için kullanılır.",
+    firstOption: "apt", 
+    secondOption: "rpm",
+    thirdOption: "dpkg",
+    fourthOption: "zyper",
+    correctAnswer: "#second-option" 
+
+},     {
+
+    //QUESTION 21
+    question: "'....' dosyası, dosya sistemi türleri ve parametreleri belirleyerek sistemdeki dosya sistemlerinin nasıl monte edileceğini belirler",
+    firstOption: "crontab",
+    secondOption: "resolv.conf", 
+    thirdOption: "fstab",
+    fourthOption: "hosts",
+    correctAnswer: "#third-option"
+}
+]
+
+
+
+
+
+
+
+
+
+
